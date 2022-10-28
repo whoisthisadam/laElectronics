7389368252abcaeed029cc2d0387be746cd25599
Adam Kasperovich <adamkasper16@yandex.com>
2022-10-28 18:45:59 +0300

    add some description into API

--- api/src/main/java/com/kasperovich/controller/RolesController.java
+++ api/src/main/java/com/kasperovich/controller/RolesController.java
@@ -2,12 +2,10 @@ package com.kasperovich.controller;
 
 import com.kasperovich.controller.responses.RoleResponse;
 import com.kasperovich.mapper.RoleListMapper;
-import com.kasperovich.mapper.RoleMapper;
-import com.kasperovich.models.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,24 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kasperovich.repository.RoleRepository;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @Validated
 @Slf4j
 @RequestMapping("/rest/data/roles")
 @RequiredArgsConstructor
+@Api(tags = {"Roles"})
 public class RolesController {
 
 
     private final RoleRepository roleRepository;
 
-    private final RoleMapper roleMapper;
-
     private final RoleListMapper roleListMapper;
 
     @GetMapping
+    @ApiOperation(value = "Finding all roles")
     public ResponseEntity<List<RoleResponse>>findAll(){
         List<RoleResponse>roleResponseList=roleListMapper.toResponsesList(roleRepository.findAll());
         return ResponseEntity.ok(roleResponseList);
