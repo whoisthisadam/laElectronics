4b5c9b93dd03a7f70c5bac8b41980f47b687744e
Adam Kasperovich <adamkasper16@yandex.com>
2022-11-04 15:16:27 +0200

    delete useless log4j.properties

--- api/src/main/java/com/kasperovich/DeSoccerApplication.java
+++ api/src/main/java/com/kasperovich/DeSoccerApplication.java
@@ -2,7 +2,10 @@ package com.kasperovich;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication(scanBasePackages = "com.kasperovich")
 public class DeSoccerApplication {
     public static void main(String[] args) {
