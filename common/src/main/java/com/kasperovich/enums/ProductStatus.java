d0d59b894d875961b8be5a6c4411353954bb8020
Adam Kasperovich <adamkasper16@yandex.com>
2022-11-07 11:46:02 +0200

    add product status&enum for it

---
+++ common/src/main/java/com/kasperovich/enums/ProductStatus.java
@@ -0,0 +1,7 @@
+package com.kasperovich.enums;
+
+public enum ProductStatus {
+
+    IN_STOCK, OUT_OF_STOCK, RUNNING_LOW
+
+}
---
+++ common/src/main/resources/db/migration/V1_10__add_product_status.sql
@@ -0,0 +1,2 @@
+alter table boots.products
+    add status varchar(100) not null;
