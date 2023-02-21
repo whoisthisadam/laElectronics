ddf0a4dae60c6ff35430a0b82e3e4019498a81c4
Adam Kasperovich <adamkasper16@yandex.com>
2023-02-20 23:31:46 -0500

    tests init

---
+++ api/src/test/java/com/kasperovich/desoccer/DeSoccerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kasperovich.desoccer;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
