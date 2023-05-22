133b35a7c5dd50ece0378a13b4de6e4d29e434b0
Adam Kasperovich <adamkasper24@gmail.com>
2023-05-22 06:20:29 -0400

    get address by user email db query

--- common/src/main/java/com/kasperovich/laelectronics/repository/AddressRepository.java
+++ common/src/main/java/com/kasperovich/laelectronics/repository/AddressRepository.java
@@ -2,9 +2,13 @@ package com.kasperovich.laelectronics.repository;
 
 import com.kasperovich.laelectronics.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
-    Address findByPostcode(String postcode);
+    @Query(value = "select a from Address a inner join User u on a.id=u.address.id where u.email=?1")
+    Address findAddressByUserEmail(String email);
 
 }
