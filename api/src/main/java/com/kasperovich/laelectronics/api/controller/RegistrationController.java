bb90faadd182bc25b2bb9be4af95afbb1d31693f
Adam Kasperovich <adamkasper24@gmail.com>
2023-05-26 10:25:40 -0400

    enable registration from different host

--- api/src/main/java/com/kasperovich/laelectronics/api/controller/RegistrationController.java
+++ api/src/main/java/com/kasperovich/laelectronics/api/controller/RegistrationController.java
@@ -19,10 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
@@ -33,6 +30,7 @@ import java.util.Map;
 @RequestMapping("/registration")
 @RequiredArgsConstructor
 @Tag(name = "Registration")
+@CrossOrigin
 public class RegistrationController {
 
     private final UserService userService;
